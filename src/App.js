2e34c65d28da15a005bfb7c00d69e9f53075319e
08pixels <luizrodrigomarques@gmail.com>
2019-10-01 21:13:44 -0300

    separing button as component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import CustomButton from './CustomButton';
 
 export default class Calculator extends Component {
   
   constructor(props) {
     super(props);
-    this.state = { opened: false, display: ''};
-
+    
     this.buttons = [
       ['C', '()', 'del', '/'],
       ['7', '8', '9', 'x'],
       ['4', '5', '6', '-'],
       ['1', '2', '3', '+'],
       ['+/-', '0', '.', '=']
-    ]
+    ];
+
+    this.state = {
+      opened: false,
+      display: '',
+    };
+
   }
 
-  changeDisplay(currentDisplay, value, opened) {
+  update(value) {
+    this.setState({
+      display: this.getResult(this.state.display, value, this.state.opened).toString(),
+      opened: value == '()' ? !this.state.opened : this.state.opened,
+    })
+  }
+
+  getResult(currentDisplay, value, opened) {
     if(value == 'C')
       return '';
 
@@ -42,60 +55,33 @@ export default class Calculator extends Component {
     return (currentDisplay + value).replace(/\x\x/g, '^');
   }
 
-  
-
-  customButton(value, currentStyle) {
-    return (
-      <View style={currentStyle}>
-        <TouchableOpacity
-          style={styles.containerButton}
-          onPress={() => { this.setState({
-            display: this.changeDisplay(this.state.display, value, this.state.opened).toString(),
-            opened: value == '()' ? !this.state.opened : this.state.opened,
-          })}}
-        >
-          <Text style={styles.text}> {value} </Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
   render() {
     return (
       <View style={styles.main}>
+        
         <View style={styles.display}>
-          <Text style={styles.text}>
-              {this.state.display}
-          </Text>
+          <Text style={styles.text}> {this.state.display} </Text>
         </View>
 
-        {
-          this.buttons.map(line => (
-            <View style={styles.container}>
-              { line.map(value => this.customButton(value, styles.normalButton) ) }
+        { this.buttons.map(line => (
+            <View
+            key={line.join('')}
+            style={styles.container}
+            >
+              {
+                line.map(value => (
+                  <CustomButton
+                  key={value}
+                  value={value}
+                  color='757575'
+                  update={() => this.update(value)}
+                  />
+                ))
+              }
             </View>)
           )
         }
 
-        {/* <View style={styles.container}>
-          { this.buttons[0].map(value => this.customButton(value, styles.darkButton)) }
-        </View>
-
-        <View style={styles.container}>
-          { this.buttons[1].map(value => this.customButton(value, styles.normalButton)) }
-        </View>
-
-        <View style={styles.container}>
-          { this.buttons[2].map(value => this.customButton(value, styles.normalButton)) }
-        </View>
-
-        <View style={styles.container}>
-          { this.buttons[3].map(value => this.customButton(value, styles.normalButton)) }          
-        </View>
-
-        <View style={styles.container}>
-          { this.buttons[4].map(value => this.customButton(value, styles.normalButton)) }
-        </View> */}
       </View>
     );
   }
@@ -108,26 +94,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-  },
-  containerButton: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  normalButton: {
-    flex: 1,
-    backgroundColor: '#757575',
-    borderWidth: 0.5,
-  },
-  darkButton: {
-    flex: 1,
-    backgroundColor: '#494949',
-    borderWidth: 0.5,
-  },
-  highlightButton: {
-    flex: 1,
-    backgroundColor: '#1c313a',
-    borderWidth: 0.5,
   },
   display: {
     flex: 4,
